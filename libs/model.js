cf355dfb073011345f986fb8d4bb70c947004ccb
Elbert-Ainstein <83158868+Elbert-Ainstein@users.noreply.github.com>
2022-01-17 17:28:43 +0800

    libraries

---
+++ libs/model.js
@@ -0,0 +1,37 @@
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+
+export function loadGLTFModel (
+    scene,
+    glbPath,
+    options = {recieveShadow: true, castShadow: true}
+) {
+    const { recieveShadow, castShadow } = options;
+    return new Promise((resolve, reject) => {
+        const loader = new GLTFLoader()
+
+        loader.load(
+            glbPath,
+            gltf => {
+                const obj = gltf.scene
+                obj.name = 'raygun'
+                obj.position.y = 0;
+                obj.position.x = 0;
+                obj.recieveShadow = recieveShadow;
+                obj.castShadow = castShadow;
+                scene.add(obj)
+
+                obj.traverse(function (child) {
+                    if(child.isMesh) {
+                        child.castShadow = castShadow;
+                        child.receiveShadow = recieveShadow;
+                    }
+                });
+                resolve(obj);
+            },
+            undefined,
+            function(error) {
+                reject(error);
+            }
+        )
+    })
+}
---
+++ libs/theme.js
@@ -0,0 +1,60 @@
+import { extendTheme } from '@chakra-ui/react'
+import { mode } from '@chakra-ui/theme-tools'
+
+const styles = {
+  global: props => ({
+    body: {
+      bg: mode('#f0e7db', '#202023')(props)
+    }
+  })
+}
+
+const components = {
+  Heading: {
+    variants: {
+      'section-title': {
+        textDecoration: 'underline',
+        fontSize: 20,
+        textUnderlineOffset: 6,
+        textDecorationColor: '#525252',
+        textDecorationThickness: 4,
+        marginTop: 3,
+        marginBottom: 4
+      }
+    }
+  },
+  Link: {
+    baseStyle: props => ({
+      color: mode('#3d7aed', '#ff63c3')(props),
+      textUnderlineOffset: 3
+    })
+  }
+}
+
+const fonts = {
+  heading: "'M PLUS Rounded 1c'"
+}
+
+const colors = {
+  grassTeal: '#88ccca',
+  teal: {
+        50: '#e6fffa',
+        100: 'b2f5ea',
+        200: '81e6d9',
+        300: '4fd1c5',
+        400: '38b2ac',
+        500: '319795',
+        600: '2c7a7b',
+        700: '285e61',
+        800: '234e52',
+        900: '1d3557'
+  }
+}
+
+const config = {
+  initialColorMode: 'dark',
+  useSystemColorMode: true
+}
+
+const theme = extendTheme({ config, styles, components, fonts, colors })
+export default theme
